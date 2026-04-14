aa1e4b3dc4a0657aebd8bd9731abee49dce1352a
GabrielVerdin <gabrielg.verdin@gmail.com>
2026-04-14 15:13:16 -0300

    seleção de colunas uteis e exclusao de linhas totalmente vazias

--- rel_compras.py
+++ rel_compras.py
@@ -14,12 +14,16 @@ caminho_arquivo = filedialog.askopenfilename(
 # Lê o arquivo usando '\t' como separador
 rel_compras = pd.read_excel(caminho_arquivo, engine="xlrd")
 
+# Seleciona somente as colunas desejadas (F, L, M e S) e renomeia para algo mais explicativo
+colunas_desejadas = rel_compras.iloc[:, [5, 11, 12, 18]]
+colunas_desejadas.columns = ["Fornecedor", "Cod_Produto", "Desc_Produto", "Qtd_pedida"]
+
 # Remove linhas totalmente vazias
-rel_compras = rel_compras.dropna(axis=1, how='all')
+colunas_desejadas = colunas_desejadas.dropna(how='all')
 
 # Printa as primeiras informações do arquivo 
 print("Arquivo selecionado: ", caminho_arquivo)
-print(rel_compras.head())
+print(colunas_desejadas.head())
 
 # Exporta o arquivo como csv
 rel_compras.to_csv('estoque_bruto.csv', index=False, encoding="utf-8")
