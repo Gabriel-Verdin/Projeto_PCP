8b056318f3c7fe954660430e7a834c2be458e877
GabrielVerdin <gabrielg.verdin@gmail.com>
2026-04-14 11:03:39 -0300

    adição de comentarios no codigo

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pandas as pd
 import tkinter as tk 
 from tkinter import filedialog
 
+# Abre o explorador de arquivos
 root = tk.Tk()
 root.withdraw()
 
@@ -10,6 +11,7 @@ caminho_arquivo = filedialog.askopenfilename(
     filetypes=[("Arquivos Excel", "*.xlsx *.xls")]
 )
 
+# Lê o arquivo usando ';' como separador
 df = pd.read_csv(caminho_arquivo, sep=';', encoding='latin1', header=None)
 
 print("Arquivo Selecionado: ", caminho_arquivo)
