2c119def30f50b0db36286c7c9d572d49f28a95d
GabrielVerdin <gabrielg.verdin@gmail.com>
2026-04-15 17:20:09 -0300

    adiciona script para importar e processar arquivos Excel, incluindo limpeza de dados e exportação para CSV

---
+++ estrutura.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import tkinter as tk
+from tkinter import filedialog
+
+root = tk.Tk()
+root.withdraw()
+
+caminho_arquivo = filedialog.askopenfilename(
+   title="Selecione o Arquivo",
+    filetypes=[("Arquivos Excel", "*.xlsx *.xls")]
+)
+
+data_frame = pd.read_csv(caminho_arquivo, sep='\t', encoding='latin1', engine='python')
+
+# Remove linhas totalmente vazias
+data_frame = data_frame.dropna(how='all')
+
+data_frame.iloc[:, :4]
+data_frame.columns = ['Nivel', 'Codigo', 'Descricao', 'Quantidade']
+
+print(data_frame)
+data_frame.to_csv('teste.csv', index=False, encoding='utf-8')
