15c87d0db5a97ed9a79b8e88d48e9cca1c38f848
GabrielVerdin <gabrielg.verdin@gmail.com>
2026-04-16 13:59:39 -0300

    adiciona função para importar relatórios de compras para o banco de dados e organiza a conexão

--- teste.py
+++ teste.py
@@ -1,16 +1,23 @@
 import importar_relatorio
 import sqlite3
 
-relatorio = importar_relatorio.importar_relatorio_generico()
+# Fução de Importação do Relatório para o Banco de Dados
+def db_relatorio_compras(conexao):
+    relatorio_compras = importar_relatorio.importar_relatorio_generico()
+    relatorio_compras.to_sql("relatorio_compras", conexao, if_exists="replace", index=False)
 
+# Conexão com o Banco de Dados
 conexao = sqlite3.connect('meu_banco.db')
 cursor = conexao.cursor()
 
-relatorio.to_sql("relatorio_compras", conexao, if_exists="replace", index=False)
+# Chamada da Função
+db_relatorio_compras(conexao)
 
+# Consulta do Banco de Dados
 cursor.execute("SELECT * FROM relatorio_compras")
 for linha in cursor.fetchall():
     print(linha)
 
+# Fechamento da Conexão
 conexao.commit()
 conexao.close()
